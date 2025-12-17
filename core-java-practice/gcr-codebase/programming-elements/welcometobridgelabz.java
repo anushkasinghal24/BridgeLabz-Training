73e6c3a259b42c1067a216f3c8ec0949e54f8dd0
anushkasinghal24 <anushkasinghal233@gmail.com>
2025-12-17 15:26:49 +0530

    Create welcometobridgelabz.java

---
+++ core-java-practice/gcr-codebase/programming-elements/welcometobridgelabz.java
@@ -0,0 +1,7 @@
+//Write a program that prints "Welcome to BridgeLabz" on the screen.
+
+public class welcometobridgelabz {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz");
+    }
+}
