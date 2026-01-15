9cbf04df57a914dad117e98cafd98acc4ed7ff61
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-01-15 22:17:06 +0530

    [Anushka Singhal] Add .Added Leetcode questions

---
+++ dsa-practice/leet-code-codebase/RemoveDuplicates.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveDuplicates {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                // Skip the duplicate node
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    // Helper method to print the list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // Creating sorted linked list: 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        RemoveDuplicates rd = new RemoveDuplicates();
+        head = rd.deleteDuplicates(head);
+
+        printList(head); // Output: 1 -> 2 -> 3 -> null
+    }
+}
