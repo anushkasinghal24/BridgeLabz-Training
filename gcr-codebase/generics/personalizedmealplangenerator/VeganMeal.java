fc199ec29bae4317b62f6798b23c828d4f9baf34
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-01-21 11:08:47 +0530

    [Anushka Singhal] Add .Added Personalised Meal generator Problem.

---
+++ gcr-codebase/generics/personalizedmealplangenerator/VeganMeal.java
@@ -0,0 +1,16 @@
+package personalizedmealplangenerator;
+
+public class VeganMeal implements MealPlan{
+
+    @Override
+
+    public String getMealType(){
+        return "Vegan Meal";
+    }
+
+    @Override
+
+    public void showMeal(){
+        System.out.println("meal : vegetables, brown rice, fruits, tofu");
+    }
+}
