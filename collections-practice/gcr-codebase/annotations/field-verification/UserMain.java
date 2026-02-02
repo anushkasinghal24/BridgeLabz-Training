42e68e4698466d42a609f42c71c180bf536fdc6a
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-02-02 16:37:32 +0530

    [Anushka Singhal] Add .Added Annotations Questions

---
+++ collections-practice/gcr-codebase/annotations/field-verification/UserMain.java
@@ -0,0 +1,9 @@
+public class UserMain{
+    public static void main(String args[]){
+        User user1 = new User("a123");
+        System.out.println("User created "+user1.getUsername());
+
+        User user2 = new User("Hello Program");
+
+    }
+}
