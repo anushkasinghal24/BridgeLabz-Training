830df40acaa0b3b41c3aeca79168cbb742caf306
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-02-05 12:29:47 +0530

    [Anushka Singhal] Add .Added Functional Interfaces Questions

---
+++ java-8-features-practice/gcr-codebase/functional-interfaces/data-export-feature/Exporter.java
@@ -0,0 +1,6 @@
+interface Exporter{
+    void export();
+    default void exportToJson(){
+        System.out.println("Exporting data to JSON");
+    }
+}
---
+++ java-8-features-practice/gcr-codebase/functional-interfaces/digital-payment-interface/SmartDevice.java
@@ -0,0 +1,5 @@
+
+interface SmartDevice{
+    void turnOn ();
+    void turnOff();
+}
