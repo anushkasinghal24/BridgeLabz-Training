14c7c530ec4d6fc9383be2a21ab63de7b635e191
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-02-21 15:21:50 +0530

    [Anushka Singhal] Add .Added Scenario-based question

---
+++ collections-practice/scenario-based/SmartTrafficCollectionSystem/InvalidVehicleException.java
@@ -0,0 +1,7 @@
+package SmartTrafficCollectionSystem;
+
+public class InvalidVehicleException extends Exception{
+    public InvalidVehicleException(String message){
+        super(message);
+    }
+}
---
+++ collections-practice/scenario-based/SmartTrafficCollectionSystem/SignalJumpViolation.java
@@ -0,0 +1,12 @@
+package SmartTrafficCollectionSystem;
+public class SignalJumpViolation extends Violation{
+    public SignalJumpViolation(){
+        super("Signal Jump");
+    }
+    @Override
+    public int calculateFine(int previousViolation){
+        int fine=700;
+        if(previousViolation>0) fine=fine+300;
+        return fine;
+    }
+}
