8b881ccacef0d9cf03901bdb5434174ff403a213
anushkasinghal24 <anushkasinghal233@gmail.com>
2026-08-12 20:39:25 +0530

    [Anushka] Add .Added day-8

--- Refresher-Training/Day-8/project/src/main/java/com/bridgelabz/project/controller/StudentController.java
+++ Refresher-Training/Day-8/project/src/main/java/com/bridgelabz/project/controller/StudentController.java
@@ -1,4 +1,61 @@
 package com.bridgelabz.project.controller;
 
+import com.bridgelabz.project.dto.StudentDTO;
+import com.bridgelabz.project.entity.Student;
+import com.bridgelabz.project.service.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // GET all students
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // GET student by ID
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Integer id) {
+        return studentService.getStudentById(id);
+    }
+
+//    // POST student
+//    @PostMapping
+//    public Student addStudent(@Valid @RequestBody Student student) {
+//        return studentService.addStudent(student);
+//    }
+
+    // PUT student
+    @PutMapping("/{id}")
+    public Student updateStudent(
+            @PathVariable Integer id,
+            @Valid @RequestBody Student student) {
+
+        return studentService.updateStudent(id, student);
+    }
+
+    // DELETE student
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Integer id) {
+
+        studentService.deleteStudent(id);
+
+        return "Student deleted successfully";
+    }
+
+
+    @PostMapping
+    public Student addStudent(@Valid @RequestBody StudentDTO dto){
+        return studentService.addStudent(dto);
+    }
 }
--- Refresher-Training/Day-8/project/src/main/java/com/bridgelabz/project/service/StudentService.java
+++ Refresher-Training/Day-8/project/src/main/java/com/bridgelabz/project/service/StudentService.java
@@ -1,4 +1,84 @@
 package com.bridgelabz.project.service;
 
+import com.bridgelabz.project.dto.StudentDTO;
+import com.bridgelabz.project.entity.Student;
+import com.bridgelabz.project.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    // Get all students
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    // Get student by ID
+    public Student getStudentById(Integer id) {
+        return studentRepository.findById(id)
+                .orElse(null);
+    }
+
+    // Add student
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    // Update student
+    public Student updateStudent(Integer id, Student student) {
+
+        Student existingStudent = studentRepository.findById(id)
+                .orElse(null);
+
+        if (existingStudent != null) {
+
+            existingStudent.setRollNumber(student.getRollNumber());
+            existingStudent.setFirstName(student.getFirstName());
+            existingStudent.setLastName(student.getLastName());
+            existingStudent.setGender(student.getGender());
+            existingStudent.setEmail(student.getEmail());
+            existingStudent.setAddress(student.getAddress());
+            existingStudent.setCity(student.getCity());
+            existingStudent.setState(student.getState());
+            existingStudent.setCourse(student.getCourse());
+            existingStudent.setDepartment(student.getDepartment());
+            existingStudent.setAdmissionYear(student.getAdmissionYear());
+
+            return studentRepository.save(existingStudent);
+        }
+
+        return null;
+    }
+
+    // Delete student
+    public void deleteStudent(Integer id) {
+        studentRepository.deleteById(id);
+    }
+
+
+    public Student addStudent(StudentDTO dto){
+        Student student = new Student();
+        student.setRollNumber(dto.getRollNumber());
+        student.setFirstName(dto.getFirstName());
+        student.setLastName(dto.getLastName());
+        student.setGender(dto.getGender());
+        student.setEmail(dto.getEmail());
+        student.setAddress(dto.getAddress());
+        student.setCity(dto.getCity());
+        student.setState(dto.getState());
+        student.setCourse(dto.getCourse());
+        student.setDepartment(dto.getDepartment());
+        student.setAdmissionYear(dto.getAdmissionYear());
+
+        return studentRepository.save(student);
+
+    }
 }
